9af7720f002c11149070f8df2d1a379250167849
Antonio Cecere <antocecere77@gmail.com>
2019-11-21 07:08:00 +0100

    Aggiunte azioni di update e delete

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -22,6 +22,27 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
           ...state, // Copy old state
           ingredients: [...state.ingredients, ...action.payload]
         };
+      case ShoppingListActions.UPDATE_INGREDIENT:
+        const ingredient = state.ingredients[action.payload.index];
+        const updatedIngredient = {
+          ...ingredient, // Copy all ingredient properties
+          ...action.payload.ingredient // Overwrite properties
+        };
+
+        const updateIngredients = [...state.ingredients];
+        updateIngredients[action.payload.index] = updatedIngredient;
+
+        return {
+            ...state,
+            ingredients: updateIngredients
+          };
+      case ShoppingListActions.DELETE_INGREDIENT:
+        return {
+          ...state,
+          ingredients: state.ingredients.filter((ig, igIndex) => {
+            return igIndex !== action.payload;
+          })
+        };
       default:
         return state;
   }
