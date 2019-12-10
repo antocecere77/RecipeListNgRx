3920eb9f770ce5cf74fbaf7792307c6fcb7c9f76
antocecere <antocecere77@gmail.com>
2019-12-10 06:57:33 +0100

    Removed shopping list  service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription, Observable } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
 import { LoggingService } from '../logging.service';
 import { Store } from '@ngrx/store';
 import * as fromShoppingList from './store/shopping-list.reducer';
@@ -17,8 +16,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Observable<{ ingredients: Ingredient[]}>;
   private subscription: Subscription;
 
-  constructor(
-    private slService: ShoppingListService,
+  constructor(   
     private loggingService: LoggingService,
     private store: Store<fromShoppingList.AppState>
   ) {}
